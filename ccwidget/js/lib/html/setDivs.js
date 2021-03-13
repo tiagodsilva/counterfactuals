6f495bc5f8c24a0f2eb899933bd05768b44c1137
tdsh-ux <tdsh97@gmail.com>
2021-03-13 16:51:36 -0300

    Update margin

--- ccwidget/js/lib/html/setDivs.js
+++ ccwidget/js/lib/html/setDivs.js
@@ -1,11 +1,11 @@
 export default function setDivs(el) {
 	let body = document.createElement("body");
-	body.setAttribute("style", "background-color: #EDF2F8");
+	body.setAttribute("style", "background-color: #EDF2F8;overflow-x:scroll");
 
 	body.innerHTML = `
 	<div id="container">
 			<!-- <div style="width: calc(100% / 4.2)" id="leftChart"></div> -->
-			<div align = "left" style="width: calc(100%/1.5)" id="map">
+			<div align = "left" style="width: calc(100%/1.5); margin-left:15px" id="map">
 					<div id="First_ScatterPlot" style="width: calc(100%); height: 100%; margin: 0px; display: inline-block; padding: 0px; "></div>
 					<div id="Second_ScatterPlot" style="width: calc(100% / 7); height: 100%; margin: 0px; display: inline-block; padding: 0px; "></div>
 			</div>
